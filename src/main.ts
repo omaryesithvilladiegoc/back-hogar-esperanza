c9518a9e7c29452ff700f60d8124b5b011dac286
omaryesithvilladiegoc <geriatricolaesperanza.mtr@gmail.com>
2026-04-22 19:42:45 -0500

    fix: correct CORS origin URL for frontend production

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: [ 'frontend-production-hogar-esperanza-eosin.vercel.app', 'http://localhost:3000',],
+    origin: [ 'https://frontend-production-hogar-esperanza-eosin.vercel.app', 'http://localhost:3000',],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
